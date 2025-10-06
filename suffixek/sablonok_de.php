b1e59e1443de15d10dbc73378945d4b0f95d9cb8
Könyves Andás <konyvesandras80@gmail.com>
2025-10-06 16:05:13 +0200

    Add files via upload

---
+++ suffixek/sablonok_de.php
@@ -0,0 +1,203 @@
+<?php
+return array (
+  'ig' => 
+  array (
+    'lang' => 'de',
+    'type' => 'adjective',
+  ),
+  'e' => 
+  array (
+    'lang' => 'de',
+    'type' => 'noun',
+    'number' => 'plural',
+  ),
+  's' => 
+  array (
+    'lang' => 'de',
+    'type' => 'noun',
+    'case' => 'genitive',
+  ),
+  'es' => 
+  array (
+    'lang' => 'de',
+    'type' => 'pronoun',
+    'person' => '3sg',
+  ),
+  'er' => 
+  array (
+    'lang' => 'de',
+    'type' => 'pronoun',
+    'person' => '3sg',
+  ),
+  'est' => 
+  array (
+    'lang' => 'de',
+    'type' => 'verb',
+    'tense' => 'present',
+    'person' => '2sg',
+  ),
+  'en' => 
+  array (
+    'lang' => 'de',
+    'type' => 'verb',
+    'tense' => 'infinitive',
+  ),
+  'n' => 
+  array (
+    'lang' => 'de',
+    'type' => 'noun',
+    'case' => 'dative',
+  ),
+  'in' => 
+  array (
+    'lang' => 'de',
+    'type' => 'noun',
+    'gender' => 'feminine',
+  ),
+  'innen' => 
+  array (
+    'lang' => 'de',
+    'type' => 'noun',
+    'gender' => 'feminine',
+    'number' => 'plural',
+  ),
+  'ste' => 
+  array (
+    'lang' => 'de',
+    'type' => 'adjective',
+    'degree' => 'superlative',
+  ),
+  'sten' => 
+  array (
+    'lang' => 'de',
+    'type' => 'adjective',
+    'degree' => 'superlative',
+  ),
+  'lich' => 
+  array (
+    'lang' => 'de',
+    'type' => 'adjective',
+  ),
+  'haft' => 
+  array (
+    'lang' => 'de',
+    'type' => 'adjective',
+  ),
+  't' => 
+  array (
+    'lang' => 'de',
+    'type' => 'verb',
+    'tense' => 'present',
+    'person' => '3sg',
+  ),
+  'te' => 
+  array (
+    'lang' => 'de',
+    'type' => 'verb',
+    'tense' => 'past',
+    'person' => '1sg',
+  ),
+  'ten' => 
+  array (
+    'lang' => 'de',
+    'type' => 'verb',
+    'tense' => 'past',
+    'person' => '1pl',
+  ),
+  'st' => 
+  array (
+    'lang' => 'de',
+    'type' => 'verb',
+    'tense' => 'present',
+    'person' => '2sg',
+  ),
+  'end' => 
+  array (
+    'lang' => 'de',
+    'type' => 'verb',
+    'form' => 'participle',
+  ),
+  'et' => 
+  array (
+    'lang' => 'de',
+    'type' => 'verb',
+    'tense' => 'present',
+    'person' => '3sg',
+  ),
+  'ich' => 
+  array (
+    'lang' => 'de',
+    'type' => 'pronoun',
+    'person' => '1sg',
+  ),
+  'du' => 
+  array (
+    'lang' => 'de',
+    'type' => 'pronoun',
+    'person' => '2sg',
+  ),
+  'sie' => 
+  array (
+    'lang' => 'de',
+    'type' => 'pronoun',
+    'person' => '3pl',
+  ),
+  'wir' => 
+  array (
+    'lang' => 'de',
+    'type' => 'pronoun',
+    'person' => '1pl',
+  ),
+  'ihr' => 
+  array (
+    'lang' => 'de',
+    'type' => 'pronoun',
+    'person' => '2pl',
+  ),
+  'dies' => 
+  array (
+    'lang' => 'de',
+    'type' => 'pronoun',
+  ),
+  'und' => 
+  array (
+    'lang' => 'de',
+    'type' => 'conjunction',
+  ),
+  'aber' => 
+  array (
+    'lang' => 'de',
+    'type' => 'conjunction',
+  ),
+  'doch' => 
+  array (
+    'lang' => 'de',
+    'type' => 'particle',
+  ),
+  'nicht' => 
+  array (
+    'lang' => 'de',
+    'type' => 'particle',
+  ),
+  'auch' => 
+  array (
+    'lang' => 'de',
+    'type' => 'particle',
+  ),
+  'nur' => 
+  array (
+    'lang' => 'de',
+    'type' => 'particle',
+  ),
+  'schon' => 
+  array (
+    'lang' => 'de',
+    'type' => 'particle',
+  ),
+  'ordnung' => 
+  array (
+    'lang' => 'de',
+    'type' => 'noun',
+    'compound' => true,
+  ),
+);
---
+++ suffixek/sablonok_sa.php
@@ -0,0 +1,139 @@
+<?php
+return array (
+  'ः' => 
+  array (
+    'lang' => 'sa',
+    'type' => 'noun',
+    'case' => 'nominative',
+    'number' => 'singular',
+    'gender' => 'masculine',
+  ),
+  'म्' => 
+  array (
+    'lang' => 'sa',
+    'type' => 'noun',
+    'case' => 'accusative',
+    'number' => 'singular',
+  ),
+  'ाः' => 
+  array (
+    'lang' => 'sa',
+    'type' => 'noun',
+    'case' => 'nominative',
+    'number' => 'plural',
+  ),
+  'ान्' => 
+  array (
+    'lang' => 'sa',
+    'type' => 'noun',
+    'case' => 'accusative',
+    'number' => 'plural',
+  ),
+  'स्य' => 
+  array (
+    'lang' => 'sa',
+    'type' => 'noun',
+    'case' => 'genitive',
+    'number' => 'singular',
+  ),
+  'ेषु' => 
+  array (
+    'lang' => 'sa',
+    'type' => 'noun',
+    'case' => 'locative',
+    'number' => 'plural',
+  ),
+  'ेन' => 
+  array (
+    'lang' => 'sa',
+    'type' => 'noun',
+    'case' => 'instrumental',
+    'number' => 'singular',
+  ),
+  'ाय' => 
+  array (
+    'lang' => 'sa',
+    'type' => 'noun',
+    'case' => 'dative',
+    'number' => 'singular',
+  ),
+  'ति' => 
+  array (
+    'lang' => 'sa',
+    'type' => 'verb',
+    'tense' => 'present',
+    'person' => 'third',
+    'number' => 'singular',
+  ),
+  'न्ति' => 
+  array (
+    'lang' => 'sa',
+    'type' => 'verb',
+    'tense' => 'present',
+    'person' => 'third',
+    'number' => 'plural',
+  ),
+  'ते' => 
+  array (
+    'lang' => 'sa',
+    'type' => 'pronoun',
+    'number' => 'plural',
+  ),
+  'ताम्' => 
+  array (
+    'lang' => 'sa',
+    'type' => 'verb',
+    'mood' => 'imperative',
+    'person' => 'third',
+    'number' => 'plural',
+  ),
+  'तु' => 
+  array (
+    'lang' => 'sa',
+    'type' => 'verb',
+    'mood' => 'imperative',
+    'person' => 'third',
+    'number' => 'singular',
+  ),
+  'यः' => 
+  array (
+    'lang' => 'sa',
+    'type' => 'pronoun',
+    'case' => 'nominative',
+    'number' => 'singular',
+  ),
+  'तं' => 
+  array (
+    'lang' => 'sa',
+    'type' => 'pronoun',
+    'case' => 'accusative',
+    'number' => 'singular',
+  ),
+  'तस्य' => 
+  array (
+    'lang' => 'sa',
+    'type' => 'pronoun',
+    'case' => 'genitive',
+    'number' => 'singular',
+  ),
+  'च' => 
+  array (
+    'lang' => 'sa',
+    'type' => 'conjunction',
+  ),
+  'हि' => 
+  array (
+    'lang' => 'sa',
+    'type' => 'particle',
+  ),
+  'एव' => 
+  array (
+    'lang' => 'sa',
+    'type' => 'adverb',
+  ),
+  'ऽ' => 
+  array (
+    'lang' => 'sa',
+    'type' => 'sandhi-marker',
+  ),
+);
